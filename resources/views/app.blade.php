fc40049c8368a36266e890939523ebedd2fe871c
Krzysztof Karaś <krzychu12350@interia.pl>
2025-08-17 21:02:39 +0200

    Update app name in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Evermar Kalkulator') }}</title>
         @routes
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">

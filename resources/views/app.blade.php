19995423620e9581e32445fc6be80b2c33219f7f
Krzysztof Karaś <krzychu12350@interia.pl>
2025-08-17 21:13:23 +0200

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title inertia>{{ config('app.name', 'Evermar Kalkulator') }}</title>
+        <link rel="icon" href="/android-chrome-192x192.png" type="image/x-icon">
         @routes
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
